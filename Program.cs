23b89cb57c7b78239c132c4b662b4dba84d22eb1
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-09 21:22:43 +0530

    Add : Finding Removability

--- Program.cs
+++ Program.cs
@@ -34,10 +34,10 @@ namespace HastTableDemo
             hash.Add("13","situaions");
             string hash5 = hash.Get("5");
             Console.WriteLine("5th index value :" + hash5);
-            //hash.Remove("2");
-            //string hash2 = hash.Get("2");
+            hash.Remove("2");
+            string hash2 = hash.Get("2");
 
-            //Console.WriteLine("2th index value : " + hash2);
+            Console.WriteLine("2th index value : " + hash2);
         }  
     }
 }
